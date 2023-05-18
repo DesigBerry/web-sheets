29960e166b93cfbe9ceda2a14771dd6cd3ad8245
DesigBerry <132074299+DesigBerry@users.noreply.github.com>
2023-05-18 12:24:38 -0400

    Update Signup-Page.js 1.1.9

--- Signup-Page.js
+++ Signup-Page.js
@@ -2,8 +2,7 @@
 import { initializeApp, getApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-analytics.js";
 import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
-import { doc, setDoc } from "firebase/firestore";
+import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
 //Firebase API
 const firebaseConfig = {
