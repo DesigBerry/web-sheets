b9ab147664b5629f73f5fa23f0cfc3a1fe3c9bc0
DesigBerry <132074299+DesigBerry@users.noreply.github.com>
2023-05-22 17:41:36 -0400

    Create subscribePage.js 0.0.1

---
+++ subscribePage.js
@@ -0,0 +1,36 @@
+//Firebase startup/get its shit
+import { initializeApp, getApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-analytics.js";
+import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+import { getFirestore, doc, setDoc, collection, getDocs, query, where } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+
+//Firebase API
+const firebaseConfig = {
+    apiKey: "AIzaSyBbkzuOmJcqTgfJyf2fbdciEeP59oJu0jY",
+    authDomain: "openbayautos.firebaseapp.com",
+    projectId: "openbayautos",
+    storageBucket: "openbayautos.appspot.com",
+    messagingSenderId: "244689157595",
+    appId: "1:244689157595:web:3a6c5650962e5a9eabbbfe",
+    measurementId: "G-36YCSJZJEN"
+  };
+
+//initialize Firebase
+let app;
+
+// Check if Firebase app has already been initialized
+try {
+  app = getApp();
+} catch {
+  app = initializeApp(firebaseConfig);
+}
+
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+//initialize Firestore
+const db = getFirestore();
+
+auth.onAuthStateChanged( function (user) {
+  console.log("Subscribe", userId);
+};
