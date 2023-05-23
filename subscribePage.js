f45b36e01e1f3761decb830e40f78a0d092ea604
DesigBerry <132074299+DesigBerry@users.noreply.github.com>
2023-05-23 10:13:52 -0400

    Update subscribePage.js 0.2.1

--- subscribePage.js
+++ subscribePage.js
@@ -59,5 +59,6 @@ auth.onAuthStateChanged(async function (user) {
 
 //car health button
 carHealthButton.addEventListener('click', function (event) {
-    window.location.href = `https://buy.stripe.com/test_fZebJW8OneA5dDq8wx?prefilled_email=${email}&client_reference_id=${stripeId}`;
+    //window.location.href = `https://buy.stripe.com/test_fZebJW8OneA5dDq8wx?prefilled_email=${email}&client_reference_id=${stripeId}`;
+    console.log("link", `https://buy.stripe.com/test_fZebJW8OneA5dDq8wx?prefilled_email=${email}&client_reference_id=${stripeId}`);
 });
