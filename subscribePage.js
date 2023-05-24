d2cc8f79a41b42dd8904fe50a15d212ed888d794
DesigBerry <132074299+DesigBerry@users.noreply.github.com>
2023-05-24 12:57:01 -0400

    Update subscribePage.js 0.2.4

--- subscribePage.js
+++ subscribePage.js
@@ -59,7 +59,7 @@ auth.onAuthStateChanged(async function (user) {
 
 //car health button
 carHealthButton.addEventListener('click', function (event) {
-    //window.location.href = `https://buy.stripe.com/test_fZebJW8OneA5dDq8wx?prefilled_email=${email}&client_reference_id=${stripeId}`;
-    let link = `https://buy.stripe.com/test_fZebJW8OneA5dDq8wx?prefilled_email=${email}&client_reference_id=${stripeId}`;
-    console.log("link", link);
+    window.location.href = `https://us-central1-openbayautos.cloudfunctions.net/webflowSession?prefilled_email=${email}&client_reference_id=${stripeId}`;
+//     let link = `https://buy.stripe.com/test_fZebJW8OneA5dDq8wx?prefilled_email=${email}&client_reference_id=${stripeId}`;
+//     console.log("link", link);
 });
