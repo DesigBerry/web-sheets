0a4ff6c34701c2c56c449cda62479367b498d0b5
DesigBerry <132074299+DesigBerry@users.noreply.github.com>
2023-05-31 18:00:46 -0400

    Update firebaseWholeSite.js 0.2.9

--- firebaseWholeSite.js
+++ firebaseWholeSite.js
@@ -53,7 +53,7 @@ auth.onAuthStateChanged( function (user) {
 // checkUserStatus();
 
 //function to check if user is signed in
-function checkUserStatus() {
+function checkUserStatus(user) {
   if (user) {
       // User is signed in.
       if (publicPages.includes(currentPath)) {
