4332c3272fb7330db33f53181b2b07b9fc48981a
DesigBerry <132074299+DesigBerry@users.noreply.github.com>
2023-06-05 15:35:52 -0400

    Update Signup-Page.js 1.8.7

--- Signup-Page.js
+++ Signup-Page.js
@@ -337,7 +337,7 @@ async function grabInfo1() {
 
     //put grabbed info in an array and return it
     let formData = [email.value, name.value, number.value, city.value, state.value];
-    let formMessage = "{"email":email.value,"firstname":name.value,"number":number.value,"city":city.value,"state": state.value}"; 
+    let formMessage = '{"email":email.value,"firstname":name.value,"number":number.value,"city":city.value,"state": state.value}'; 
     
     
         const response = await fetch(`https://us-central1-openbayautos.cloudfunctions.net/createHubSpotContact?data=${formMessage}`, {
