c17470fedae254239a46c99e5a964eacda42c70b
DesigBerry <132074299+DesigBerry@users.noreply.github.com>
2023-06-06 10:08:52 -0400

    Update Signup-Page.js 2.0.0

--- Signup-Page.js
+++ Signup-Page.js
@@ -131,6 +131,7 @@ signUpCarModel.options[0].value = "";
 //Grabbing information on the first form after clicking the button
 signUpButton1.addEventListener('click', function (event) {
     form1Data = grabInfo1();
+    console.log("form1Data", form1Data);
 });
 
 //create user in Firebase
@@ -338,13 +339,13 @@ async function grabInfo1() {
     //put grabbed info in an array and return it
     let formData = [email.value, name.value, number.value, city.value, state.value];
     
-//     //create a customer in HubSpot
-//     const response = await fetch(`https://us-central1-openbayautos.cloudfunctions.net/createHubSpotContact?email=${email.value}&firstname=${name.value}&number=${number.value}&city=${city.value}&state=${state.value}`, {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//     });
+    //create a customer in HubSpot
+    const response = await fetch(`https://us-central1-openbayautos.cloudfunctions.net/createHubSpotContact?email=${email.value}&firstname=${name.value}&number=${number.value}&city=${city.value}&state=${state.value}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
     
     return formData;
 }
@@ -414,13 +415,13 @@ async function createUser() {
         const querySnapshot = await getDocs(q);
         console.log("quearySnapshot", querySnapshot);
         
-//         //create a customer in HubSpot
-//         const response = await fetch(`https://us-central1-openbayautos.cloudfunctions.net/updateHubSpotContact?email=${email.value}&flag='first'&newCarYear=${carYear.value}&newCarMake=${carMake.value}&newCarModel=${carModel.value}`, {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//         });
+        //create a customer in HubSpot
+        const response = await fetch(`https://us-central1-openbayautos.cloudfunctions.net/updateHubSpotContact?email=${email.value}&flag='first'&newCarYear=${carYear.value}&newCarMake=${carMake.value}&newCarModel=${carModel.value}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
         
         //go to subscribe page
         window.location.href = "/subscribe";
